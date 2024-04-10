c0a5e6c2874f82fe0622456b51a0d9af3bb4b47d
aclararn <165197697+aclararn@users.noreply.github.com>
2024-04-09 22:31:06 -0300

    first commit

---
+++ AppPessoas/src/test/java/br/com/acrn/AppPessoas/AppPessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.acrn.AppPessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppPessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
